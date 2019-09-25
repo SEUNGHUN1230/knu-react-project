e49f5fd404bf1b3489b307c994a493e8b9bfa10c
SEUNGHUN1230 <wwiviww@naver.com>
2019-09-25 19:16:29 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,5 +19,19 @@ function App() {
     </div>
   );
 }
-
+const Header = () => {
+  return (
+    <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+           <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            2019년 강원대학교 성적
+          </Typography>
+        </Toolbar>
+      </AppBar>
+  )
+}
+export default Header;
 export default App;
