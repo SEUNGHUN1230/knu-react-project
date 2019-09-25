e1fa2d48b501e2292093705fdeef9a1870d6a9fc
SEUNGHUN1230 <wwiviww@naver.com>
2019-09-25 19:24:15 +0900

    2

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>반갑습니다. 강원대학교 학생여러분!</p>
-        
-      </header>
-    </div>
-  );
-}
 const Header = () => {
   return (
     <AppBar position="static">
@@ -32,6 +19,4 @@ const Header = () => {
       </AppBar>
   )
 }
-
-export default App;
 export default Header;
